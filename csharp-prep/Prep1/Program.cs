9f897fc279e44f780db32e653fce2b67be27c92e
Lora Chisholm <chi20006@byui.edu>
2023-01-03 12:45:18 -0700

    prep 1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Ask the user for their first name
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        //Ask the user for their last name
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        // Display the user's full name
+        Console.WriteLine("Your name is " + lastName + ", " + firstName + " " + lastName);
+
     }
 }
