f45b0d84bdc1d02220d24ba87821ab556d22b001
Lora Chisholm <chi20006@byui.edu>
2023-01-09 15:02:07 -0700

    made changes and finished programs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("Select a number");
+        Random rnd = new Random();
+        int number = rnd.Next(1, 101);
+        int guess = 0;
+
+        do
+        {
+
+            guess = int.Parse(Console.ReadLine());
+            guess++;
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != number);
+
+        Console.WriteLine("You guessed it!");
+
     }
 }
