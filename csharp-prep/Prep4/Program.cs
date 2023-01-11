c521f424bee6ef7c3a021a508231cf7bcc4b1a83
Lora Chisholm <chi20006@byui.edu>
2023-01-11 15:46:24 -0700

    fixed the loop and updated the variable match and fixed to -1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //This is where you initialize an empty list to store the numbers
+        List<int> numbers = new List<int>();
+
+
+        int input = -1;
+
+        //This is the While loop that continuously will prompt the user for input until they enter 0
+        while (input != 0)
+        {
+            Console.Write("Enter a number then press 0 to quit: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+
+
+        }
+        // while (input != 0) ;
+
+        // This Initializes a variable to store the sum of the numbers
+        int sum = 0;
+
+        // This is sum up of all the numbers in the list
+        for (int i = 0; i < numbers.Count; i++)
+
+        {
+            sum += numbers[i];
+        }
+
+
+        //This calculates the average of the numbers in the list
+        double average = (double)sum / numbers.Count;
+
+        //This find the maximum number in the list
+        int max = numbers.Max();
+
+        //This finds the minimum positive number in the list
+        int minPositive = numbers.Where(n => n > 0).Min();
+
+        //This sorts the numbers in the list
+        numbers.Sort();
+
+        //These next code lines print the results to the console
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average:" + average);
+        Console.WriteLine("Max: " + max);
+        Console.WriteLine("Min Positive: " + minPositive);
+        Console.WriteLine("Sorted List:" + string.Join(",", numbers));
+
     }
 }
